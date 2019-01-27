4a52a65464af2a46377673938a79018959d5c0fd
Sterling Hirsh <mybadluck22@gmail.com>
2019-01-27 01:14:31 -0600

    Move init code to House from App

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import House from './components/House.js';
-import Sink from './components/Sink.js';
-import Empty from './components/Empty.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      rows: 6,
-      columns: 8,
-      items: {
-      }
-    }
-
-    const that = this;
-
-    function addItem(row, column, Component) {
-      that.state.items[row][column] = <Component row={row} column={column} />;
-    }
-
-    for (let row = 0; row < this.state.rows; ++row) {
-      this.state.items[row] = {};
-      for (let column = 0; column < this.state.columns; ++column) {
-        addItem(row, column, Empty);
-      }
-    }
-
-    addItem(2, 2, Sink);
-  }
   render() {
     return (
       <div className="App">
-        <House numRows={6} numColumns={8} items={this.state.items} />
+        <House/>
       </div>
     );
   }
--- src/components/House.js
+++ src/components/House.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import './House.css';
 import '../img/grasstile.png';
+import Sink from './Sink.js';
+import Empty from './Empty.js';
 
 class House extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rows: 6,
+      columns: 8,
+      items: {
+      }
+    }
+
+    const that = this;
+
+    function addItem(row, column, Component) {
+      that.state.items[row][column] = <Component row={row} column={column} />;
+    }
+
+    for (let row = 0; row < this.state.rows; ++row) {
+      this.state.items[row] = {};
+      for (let column = 0; column < this.state.columns; ++column) {
+        addItem(row, column, Empty);
+      }
+    }
+
+    addItem(2, 2, Sink);
+  }
+
   render() {
-    const rowKeys = Object.keys(this.props.items);
+    const rowKeys = Object.keys(this.state.items);
+    const score = "50%";
     return (
       <div className='House-plot'>
+        <div className='House-title'>
+          Sweat Equity
+        </div>
+        <div className='House-score'>
+          {score}
+        </div>
         <table className='House-main'>
           <tbody>
             {rowKeys.map((rowKey) =>
               <tr key={rowKey}>
-                {Object.keys(this.props.items[rowKey]).map((columnKey) =>
+                {Object.keys(this.state.items[rowKey]).map((columnKey) =>
                   <td key={columnKey} className='House-cell'>
-                    {this.props.items[rowKey][columnKey]}
+                    {this.state.items[rowKey][columnKey]}
                   </td>
                 )}
               </tr>
