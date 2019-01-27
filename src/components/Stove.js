af35cd5aa635f35799fa0ea568daaf907c9a7d75
Sterling Hirsh <mybadluck22@gmail.com>
2019-01-27 03:24:45 -0600

    Add more stove transitions

--- src/components/Stove.js
+++ src/components/Stove.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import Repairable from './Repairable.js';
-import StoveGif from '../img/Stove/stove.gif';
+import Stove1 from '../img/Stove/stove.gif';
+import Stove2 from '../img/Stove/stoveHalfout.png';
+import Stove3 from '../img/Stove/stoveThreeFourthsOut.png';
+import Stove4 from '../img/Stove/stoveOut.png';
 
 class Stove extends Component {
   render() {
     return (
       <Repairable
         conditions={[
+          'Destroyed',
+          'Broken',
+          'Damaged',
           'Working'
         ]}
         displayName='Stove'
         stateImages={[
-          StoveGif
+          Stove4,
+          Stove3,
+          Stove2,
+          Stove1
         ]}
-        stateTransitionTimes={[]}
+        stateTransitionTimes={[
+          15,
+          10,
+          10
+        ]}
         {...this.props}
       />
     );
