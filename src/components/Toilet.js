60e837fe31e59df4ab88a8d14f396cd66e6a0581
Addison Grant <addison@ifixit.com>
2019-01-27 00:23:57 -0800

    Add toilet object and use in House

---
+++ src/components/Toilet.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Repairable from './Repairable.js';
+import ToiletImg1 from '../img/Toilet/toilet1/toilet1.png';
+import ToiletImg2 from '../img/Toilet/toilet2/anim.gif';
+import ToiletImg3 from '../img/Toilet/toilet3/anim.gif';
+import ToiletImg4 from '../img/Toilet/toilet4/anim.gif';
+
+class Toilet extends Component {
+  render() {
+    return (
+      <Repairable
+        conditions={[
+          'Exploding',
+          'Fountaining',
+          'Leaking',
+          'Working'
+        ]}
+        displayName='Sink'
+        stateImages={[
+          ToiletImg4,
+          ToiletImg3,
+          ToiletImg2,
+          ToiletImg1
+        ]}
+        stateTransitionTimes={[
+          10,
+          3,
+          5
+        ]}
+        {...this.props}
+      />
+    );
+  }
+}
+
+export default Toilet;
