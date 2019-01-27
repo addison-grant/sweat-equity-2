c2b9be91817e077d98d912918a66f267219b683d
Sterling Hirsh <mybadluck22@gmail.com>
2019-01-26 19:03:35 -0600

    Get components displaying in the grid

--- src/App.js
+++ src/App.js
@@ -3,11 +3,58 @@ import React, { Component } from 'react';
 import './App.css';
 import House from './components/House.js';
 
+class Empty extends Component {
+  render() {
+    return (
+      <div className="Empty">
+        Empty<br/>{this.props.row} {this.props.column}
+      </div>
+    );
+  }
+}
+
+class Sink extends Component {
+  render() {
+    return (
+      <div className="Sink">
+        Sink<br/>{this.props.row} {this.props.column}
+      </div>
+    );
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rows: 6,
+      columns: 8,
+      items: {
+      }
+    }
+
+    const that = this;
+
+    function addItem(row, column, Component) {
+      that.state.items[row][column] = <Component row={row} column={column} />;
+    }
+
+    for (let row = 0; row < this.state.rows; ++row) {
+      this.state.items[row] = {};
+      for (let column = 0; column < this.state.columns; ++column) {
+        //this.state.items[row][column] = null;
+        addItem(row, column, Empty);
+      }
+    }
+
+    //this.state.items[2][1] = <Sink row={2} column={1} />;
+    addItem(2, 2, Sink);
+  }
   render() {
     return (
       <div className="App">
-        <House numRows={6} numColumns={8} />
+        <House numRows={6} numColumns={8} items={this.state.items} />
       </div>
     );
   }
--- src/components/House.js
+++ src/components/House.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './House.css';
 class House extends React.Component {
   render() {
-    const columns = [...Array(this.props.numColumns).keys()];
-    const rows = [...Array(this.props.numRows).keys()];
+    const rowKeys = Object.keys(this.props.items);
     return (
       <div className='House-plot'>
         <table className='House-main'>
           <tbody>
-            {rows.map((rowKey) =>
+            {rowKeys.map((rowKey) =>
               <tr key={rowKey}>
-                {columns.map((columnKey) =>
-                  <td key={columnKey} className='House-cell'></td>
+                {Object.keys(this.props.items[rowKey]).map((columnKey) =>
+                  <td key={columnKey} className='House-cell'>
+                    {this.props.items[rowKey][columnKey]}
+                  </td>
                 )}
               </tr>
             )}
